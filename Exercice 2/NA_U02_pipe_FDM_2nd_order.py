fca0a66929dfde763f07af1267b959febcb8b773
Jules Bouvet <j.bouvet99@gmail.com>
2021-04-28 16:02:37 +0200

    update

--- Exercice 2/NA_U02_pipe_FDM_2nd_order.py
+++ Exercice 2/NA_U02_pipe_FDM_2nd_order.py
@@ -1,7 +1,8 @@
 """
 Calculation of modes of a closed one-dimensional pipe (2nd order)
 """
-from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 l = 1. #tube length
 c0 = 343. #speed of sound
@@ -10,13 +11,13 @@ N = 10 #number of nodal points
 # distance between adjacent nodal points
 h = l/(N-1)
 
-x=linspace(0,l,N)
+x=np.linspace(0,l,N)
 # constructing the system matrix
 # main diagonal
-A = -2.0*diag(ones(N,dtype=float))
+A = -2.0*np.diag(np.ones(N, dtype=float))
 # adjacent diagonals
-A[arange(N-1),arange(1,N)] = 1.0
-A[arange(1,N),arange(N-1)] = 1.0
+A[np.arange(N-1),np.arange(1,N)] = 1.0
+A[np.arange(1,N),np.arange(N-1)] = 1.0
 # the first and the last row
 A[0,1] = 2.0
 A[N-1,N-2] = 2.0
@@ -32,12 +33,12 @@ A[N-1,N-2] = 2.0
 
 
 # eigenvalues and eigenvectors
-eigenval,eigenvec = eig(1/(h*h) * A)
+eigenval,eigenvec = np.linalg.eig(1/(h*h) * A)
 # eigenfrequencies
-f_m = c0/2/pi * sqrt(abs(eigenval))
+f_m = c0/2/np.pi * np.sqrt(abs(eigenval))
 
 # sorting according to the increasing frequency
-ind = argsort(f_m) #argsort gives the ordered indices
+ind = np.argsort(f_m) #argsort gives the ordered indices
 eigenvec = eigenvec[:,ind]
 eigenval = eigenval[ind]
 f_sort = f_m[ind]
@@ -46,17 +47,17 @@ print(f_sort[1:6])
 # displaying the modes - linear
 plt.figure(1)
 for i in range(1,6):
-    plot(x,eigenvec[:,i], label="mode {}".format(i))
-xlim([0,1])
-grid()
-legend(loc='lower right', ncol=2)
-show()
+    plt.plot(x,eigenvec[:,i], label="mode {}".format(i))
+plt.xlim([0,1])
+plt.grid()
+plt.legend(loc='lower right', ncol=2)
+plt.show()
 
 # displaying the modes - logarithmic
 plt.figure(2)
 for i in range(1,6):
-    plot(x,20*log10(abs(eigenvec[:,i])), label="mode {}".format(i))
-xlim([0,1])
-grid()
-legend(loc='lower right', ncol=2)
-show()
+    plt.plot(x,20*np.log10(abs(eigenvec[:,i])), label="mode {}".format(i))
+plt.xlim([0,1])
+plt.grid()
+plt.legend(loc='lower right', ncol=2)
+plt.show()
--- Exercice 2/NA_U02_pipe_FDM_4th_order.py
+++ Exercice 2/NA_U02_pipe_FDM_4th_order.py
@@ -1,7 +1,9 @@
 """
 Calculation of modes of a closed one-dimensional pipe (4th order)
 """
-from pylab import *
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 l = 1. #tube length
 c0 = 343. #speed of sound
@@ -10,10 +12,10 @@ N = 10 #number of nodal points
 # distance between adjacent nodal points
 h = l/(N-1)
 
-x=linspace(0,l,N)
+x=np.linspace(0,l,N)
 # constructing the system matrix
 
-A= diag(-5/2*ones(N))+diag(4/3*ones(N-1),1)+diag(4/3*ones(N-1),-1)+diag(-1/12*ones(N-2),-2)+diag(-1/12*ones(N-2),2)
+A= np.diag(-5/2*np.ones(N))+np.diag(4/3*np.ones(N-1),1)+np.diag(4/3*np.ones(N-1),-1)+np.diag(-1/12*np.ones(N-2),-2)+np.diag(-1/12*np.ones(N-2),2)
 
 # the first row
 A[0,0] = -2.0 
@@ -32,12 +34,12 @@ A[N-1,N-3]=0
 A[N-2,N-2]=-31/12
  
 # eigenvalues and eigenvectors
-eigenval,eigenvec = eig(1/(h*h) * A)
+eigenval,eigenvec = np.linalg.eig(1/(h*h) * A)
 # eigenfrequencies
-f_m = c0/2/pi * sqrt(abs(eigenval))
+f_m = c0/2/np.pi * np.sqrt(abs(eigenval))
 
 # sorting according to the increasing frequency
-ind = argsort(f_m) #argsort gives the ordered indices
+ind = np.argsort(f_m) #argsort gives the ordered indices
 eigenvec = eigenvec[:,ind]
 eigenval = eigenval[ind]
 f_sort = f_m[ind]
@@ -46,17 +48,17 @@ print(f_sort[1:6])
 # displaying the modes - linear
 plt.figure(1)
 for i in range(1,6):
-    plot(x,eigenvec[:,i], label="mode {}".format(i))
-xlim([0,1])
-grid()
-legend(loc='lower right', ncol=2)
-show()
+    plt.plot(x,eigenvec[:,i], label="mode {}".format(i))
+plt.xlim([0,1])
+plt.grid()
+plt.legend(loc='lower right', ncol=2)
+plt.show()
 
 # displaying the modes - logarithmic
 plt.figure(2)
 for i in range(1,6):
-    plot(x,20*log10(abs(eigenvec[:,i])), label="mode {}".format(i))
-xlim([0,1])
-grid()
-legend(loc='lower right', ncol=2)
-show()
+    plt.plot(x,20*np.log10(abs(eigenvec[:,i])), label="mode {}".format(i))
+plt.xlim([0,1])
+plt.grid()
+plt.legend(loc='lower right', ncol=2)
+plt.show()
